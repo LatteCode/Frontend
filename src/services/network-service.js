08c2f9a22f1d27380027691ce4654d563660e877
krumaska <okokrt@gmail.com>
2020-05-30 02:26:44 +0900

    Update network-service.js del test comment

--- src/services/network-service.js
+++ src/services/network-service.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const API_URL = "http://server.jaram.net:5905/";
-// const API_TEST = "http://foolminecraft.iptime.org:9999/";
 
 class NetWorkService {
   getServerTime() {
@@ -9,12 +8,5 @@ class NetWorkService {
       return response.data;
     });
   }
-  //   handleResponse(response) {
-  //     if (response.status !== 200) {
-  //       const error = response.data;
-  //       return Promise.reject(error);
-  //     }
-  //     return Promise.resolve(response);
-  //   }
 }
 export default new NetWorkService();
